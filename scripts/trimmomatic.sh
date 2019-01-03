18a8e78e85b1c87c3a2f54e56725828dd99c5d47
Picasso Vasquez <prvasquez@ucdavis.edu>
2019-01-03 13:45:02 -0500

    Update trimmomatic.sh
    Updated adapter

--- scripts/trimmomatic.sh
+++ scripts/trimmomatic.sh
@@ -9,8 +9,10 @@
 #SBATCH -p med
 #SBATCH -a 25941-26018
 
-DIR=/home/prvasque/projects/mangrove_killifish_project/raw_data/fastq/
-outdir=/home/prvasque/projects/mangrove_killifish_project/trim/data/
+DIR=./prvasquez/mangrove_killifish/data/fastq/
+outdir=./prvasquez/mangrove_killifish/data/trim/
+
+adapter=./prvasquez/mangrove_killifish/NEBnextAdapt.fa
 
 cd $DIR
 
@@ -18,5 +20,5 @@ java -jar /share/apps/Trimmomatic-0.36/trimmomatic.jar PE SRR69$SLURM_ARRAY_TASK
 	SRR69$SLURM_ARRAY_TASK_ID\_2.fastq.gz $outdir/SRR69$SLURM_ARRAY_TASK_ID\_1.qc.fq.gz \
 	$outdir/orphans/69$SLURM_ARRAY_TASK_ID\_1_se $outdir/SRR69$SLURM_ARRAY_TASK_ID\_2.qc.fq.gz \
 	$outdir/orphans/69$SLURM_ARRAY_TASK_ID\_2_se \
-	ILLUMINACLIP:/home/prvasque/projects/mangrove_killifish_project/trim/adapters/NEBnextAdapt.fa:2:40:15 \
+	ILLUMINACLIP:${adapter}:2:40:15 \
 	LEADING:2 TRAILING:2 SLIDINGWINDOW:4:2 MINLEN:25
