5cf4071c02777f3bb425a63fa868a6a87e32bf34
Picasso Vasquez <prvasquez@ucdavis.edu>
2019-01-03 13:10:29 -0500

    Update fastqdump.sh
    Updated paths for github

--- scripts/fastqdump.sh
+++ scripts/fastqdump.sh
@@ -5,11 +5,10 @@
 #SBATCH -e /home/prvasque/slurm-log/fastqdump_stderr-%j.txt
 #SBATCH -t 12:00:00
 #SBATCH -c 2
+#SBATCH --array=25941-26018
 
 module load sratoolkit
 
 DIR=./prvasquez/mangrove_killifish/data/raw_data/
 
-fastq-dump -I --split-files --gzip -o ./prvasquez/mangrove_killifish/data/fastq/ $DIR/SRR69$SLURM_ARRAY_TASK_ID.sra 
-
-
+fastq-dump -I --split-files --gzip -o ./prvasquez/mangrove_killifish/data/fastq/SRR69${SLURM_ARRAY_TASK_ID}.fastq.gz $DIR/SRR69$SLURM_ARRAY_TASK_ID.sra 
