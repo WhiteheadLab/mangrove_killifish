a38e6604a17169aad2805d422960b8a2f2455f88
Picasso Vasquez <prvasquez@ucdavis.edu>
2019-01-14 14:11:50 -0800

    Update sam_to_bam2.sh
    Updated paths

--- scripts/sam_to_bam2.sh
+++ scripts/sam_to_bam2.sh
@@ -1,19 +1,14 @@
 #!/bin/bash -l
-#SBATCH -D /home/prvasque/projects/mangrove_killifish_project/alignment/
 #SBATCH --mem=16000
-#SBATCH -o /home/prvasque/slurm-log/samtools/sambam-stdout-%j.txt
-#SBATCH -e /home/prvasque/slurm-log/samtools/sambam-stderr-%j.txt
 #SBATCH -J sam_to_bam
 #SBATCH -p high
 #SBATCH -t 12:00:00
 #SBATCH --array=25941-26018%6
 
-#sam files to bam files
-# Modified Aug 8, 2018
 
 module load samtools
 
-DIR=./prvasquez/mangrove_killifish/data
+DIR=./mangrove_killifish/data
 
 name=SRR69${SLURM_ARRAY_TASK_ID}
 echo $name
