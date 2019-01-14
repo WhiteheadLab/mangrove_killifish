de9eed9c8b91a8d22b5606a1fc3c65812ea4f3b5
Picasso Vasquez <prvasquez@ucdavis.edu>
2019-01-14 14:09:51 -0800

    Update fastqc.sh
    Updated paths

--- scripts/fastqc.sh
+++ scripts/fastqc.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -l
 #SBATCH -J fastqc
-#SBATCH -o /home/prvasque/slurm-log/fastqc-stdout-%j.txt
-#SBATCH -e /home/prvasque/slurm-log/fastqc-stderr-%j.txt
 #SBATCH --mem=6000
 #SBATCH -c 2
 #SBATCH -t 6:00:00
@@ -9,6 +7,6 @@
 
 module load fastqc
 
-DIR=./prvasquez/mangrove_killifish/data/
+DIR=./mangrove_killifish/data/
 
 fastqc -o $DIR/fastqc/ $DIR/fastq/SRR69${SLURM_ARRAY_TASK_ID}.fastq.gz
