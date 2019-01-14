561b66db9ad49506cb9757e371de9bdb25913734
Picasso Vasquez <prvasquez@ucdavis.edu>
2019-01-14 14:12:24 -0800

    Update samsort.sh
    Updated paths

--- scripts/samsort.sh
+++ scripts/samsort.sh
@@ -1,16 +1,12 @@
 #!/bin/bash -l
-#SBATCH -D /home/prvasque/projects/mangrove_killifish_project/alignment/bam/
 #SBATCH --mem=16000
-#SBATCH -o /home/prvasque/slurm-log/samtools/samsort-stdout-%j.txt
-#SBATCH -e /home/prvasque/slurm-log/samtools/samsort-stderr-%j.txt
 #SBATCH -J samsort
 #SBATCH -p high
 #SBATCH -t 12:00:00
 #SBATCH -a 25941-26018%8
 
-#samtools sort
 
-DIR=./prvasquez/mangrove_killifish/data
+DIR=./mangrove_killifish/data
 
 echo SRR69${SLURM_ARRAY_TASK_ID}
 
