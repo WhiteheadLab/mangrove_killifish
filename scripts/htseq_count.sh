056630060a9a4a100f907625a189d3b5a8d7b0a5
Picasso Vasquez <prvasquez@ucdavis.edu>
2019-01-14 14:11:08 -0800

    Update htseq_count.sh
    Updated paths

--- scripts/htseq_count.sh
+++ scripts/htseq_count.sh
@@ -1,27 +1,20 @@
 #!/bin/bash -l
-#SBATCH -D /home/prvasque/projects/mangrove_killifish_project/scripts/
 #SBATCH --mem=16000
-#SBATCH -o /home/prvasque/slurm-log/htseq/hts_count-stdout-%j.txt
-#SBATCH -e /home/prvasque/slurm-log/htseq/hts_count-stderr-%j.txt
 #SBATCH -J hts_count
 #SBATCH -p high
 #SBATCH -t 12:00:00
 #SBATCH -a 25941-26018%10
 
 
-#module load pysam
-#module load python
-#module load HTSeq
-
 module load bio
 
 echo SRR${SLURM_ARRAY_TASK_ID}
 
-DIR=./prvasquez/mangrove_killifish/data/sorted
+DIR=./mangrove_killifish/data/sorted
 
-REF_DIR=./prvasquez/mangrove_killifish/data/ref
+REF_DIR=./mangrove_killifish/data/ref
 
-OUT_DIR=./prvasquez/mangrove_killifish/data/counts
+OUT_DIR=./mangrove_killifish/data/counts
 
 htseq-count --type=gene -i Dbxref -f bam -s no $DIR/SRR69${SLURM_ARRAY_TASK_ID}_sorted.bam \
 	$REF_DIR/GCF_001649575.1_ASM164957v1_genomic.gff > \
