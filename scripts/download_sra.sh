1b64b9a5a9b8b9a69937253bec8fec71e3cf30ec
Picasso Vasquez <prvasquez@ucdavis.edu>
2019-01-14 14:09:25 -0800

    Update download_sra.sh
    Updated paths

--- scripts/download_sra.sh
+++ scripts/download_sra.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -l
 #SBATCH --mem=16000
 #SBATCH -J download_sra
-#SBATCH -D /home/prvasque/projects/mangrove_killifish_project/scripts
-#SBATCH -o /home/prvasque/slurm-log/download_sra_stdout-%j.txt
-#SBATCH -e /home/prvasque/slurm-log/download_sra_stderr-%j.txt
+
+
 
 module load sratoolkit
 
