7f5bcf3d83c5a0ecf0cac118de2007a6fd25f633
Picasso Vasquez <prvasquez@ucdavis.edu>
2019-02-13 00:03:26 -0800

    Update starindex_korea_latest.sh

--- scripts/starindex_korea_latest.sh
+++ scripts/starindex_korea_latest.sh
@@ -9,10 +9,8 @@ module load star/2.4.2a
 
 DIR=./mangrove_killifish/data/ref
 
-cd $DIR
-
-STAR --runMode genomeGenerate --genomeDir $DIR --genomeFastaFiles GCF_001649575.1_ASM164957v1_genomic.fna \
-	--sjdbGTFtagExonParentTranscript Parent --sjdbGTFfile GCF_001649575.1_ASM164957v1_genomic.gff \
+STAR --runMode genomeGenerate --genomeDir $DIR --genomeFastaFiles $DIR/GCF_001649575.1_ASM164957v1_genomic.fna \
+	--sjdbGTFtagExonParentTranscript Parent --sjdbGTFfile $DIR/GCF_001649575.1_ASM164957v1_genomic.gff \
 	--sjdbOverhang 99
 
 echo "genome indexed"
