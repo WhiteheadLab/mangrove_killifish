b0a427eb89d30b55e80a9e0109f0042cba33da4f
Picasso Vasquez <prvasquez@ucdavis.edu>
2019-02-12 21:37:45 -0800

    Update trimmomatic.sh

--- scripts/trimmomatic.sh
+++ scripts/trimmomatic.sh
@@ -6,15 +6,15 @@
 #SBATCH -p med
 #SBATCH -a 25941-26018
 
-DIR=./mangrove_killifish/data/fastq/
+DIR=./mangrove_killifish/data/fastq
 outdir=./mangrove_killifish/data/trim
 
-adapter=../../files/NEBnextAdapt.fa
+adapter=./mangrove_killifish/files/NEBnextAdapt.fa
 
-cd $DIR
 
-java -jar /share/apps/Trimmomatic-0.36/trimmomatic.jar PE SRR69$SLURM_ARRAY_TASK_ID\_1.fastq.gz \
-	SRR69$SLURM_ARRAY_TASK_ID\_2.fastq.gz $outdir/SRR69$SLURM_ARRAY_TASK_ID\_1.qc.fq.gz \
+
+java -jar /share/apps/Trimmomatic-0.36/trimmomatic.jar PE $DIR/SRR69$SLURM_ARRAY_TASK_ID\_1.fastq.gz \
+	$DIR/SRR69$SLURM_ARRAY_TASK_ID\_2.fastq.gz $outdir/SRR69$SLURM_ARRAY_TASK_ID\_1.qc.fq.gz \
 	$outdir/orphans/69$SLURM_ARRAY_TASK_ID\_1_se $outdir/SRR69$SLURM_ARRAY_TASK_ID\_2.qc.fq.gz \
 	$outdir/orphans/69$SLURM_ARRAY_TASK_ID\_2_se \
 	ILLUMINACLIP:${adapter}:2:40:15 \
